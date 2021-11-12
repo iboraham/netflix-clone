861eee750d5c924d50e66cffff7feef42e339e00
Onur Şerbetçi <onurserbetcii@gmail.com>
2021-11-13 01:33:34 +0300

    Jumbotron landing page + Initial Upload react env

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import jumboData from './fixtures/jumbo';
+import Jumbotron from "./components/jumbotron"
+
+
+export default function App() {
+  return (
+    <Jumbotron.Container>
+      {jumboData.map((item) => (
+        <Jumbotron key={item.id} direction={item.direction}>
+          <Jumbotron.Pane>
+          <Jumbotron.Title>{item.title}</Jumbotron.Title>
+          <Jumbotron.subTitle>{item.subTitle}</Jumbotron.subTitle>
+          </Jumbotron.Pane>
+          <Jumbotron.Pane>
+          <Jumbotron.Image src={item.image} alt={item.alt} />
+          </Jumbotron.Pane>
+        </Jumbotron>
+      ))}
+    </Jumbotron.Container>
+
+  );
+}
+
+
